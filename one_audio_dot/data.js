1d1880cbbeb397793e4c56f091624e9000d9dac5
Molly Jones <mojones.e@gmail.com>
2026-08-24 11:53:57 -0400

    line up dots at left of canvas to start

--- one_audio_dot/data.js
+++ one_audio_dot/data.js
@@ -1,4 +1,7 @@
-export const dots = [
-    { id: "dot1", x: 0.5, y: 0.5, audioSrc: "./audio/boring_song.mp3" },
-    { id: "dot2", x: 0.25, y: 0.25, audioSrc: "./audio/rainbow.wav" },
+const leftOffset = 75;
+const numDots = 8;
+
+export const dotDefs = [
+    { id: "dot1", audioSrc: "./audio/boring_song.mp3", leftOffsetPx: leftOffset, yFraction: 1 / numDots },
+    { id: "dot2", audioSrc: "./audio/rainbow.wav", leftOffsetPx: leftOffset, yFraction: 2 / numDots },
 ];
--- one_audio_dot/interaction.js
+++ one_audio_dot/interaction.js
@@ -1,9 +1,18 @@
 import { resizeCanvas, createDot, positionDot } from "./utils.js";
-import { dots } from "./data.js";
+import { dotDefs } from "./data.js";
 
 console.clear();
 
 const container = document.querySelector(".workspace");
+const rect = container.getBoundingClientRect();
+
+// position dots initially
+const dots = dotDefs.map((def) => ({
+  id: def.id,
+  audioSrc: def.audioSrc,
+  x: def.leftOffsetPx / rect.width,
+  y: def.yFraction,
+}));
 
 // generate dots
 dots.forEach((dotData) => createDot(container, dotData));
